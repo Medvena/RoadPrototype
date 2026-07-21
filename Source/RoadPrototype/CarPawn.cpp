9c1682c2fdef6bfc6ce84df87527e47421b1042f
Ema <medvena@medvena.net>
2026-07-21 15:14:57 +0200

    🎉 Car Pawn Setup
    Create the car pawn setup + gamemode setup + level setup

---
+++ Source/RoadPrototype/CarPawn.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CarPawn.h"
+
+// Sets default values
+ACarPawn::ACarPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACarPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACarPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACarPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
